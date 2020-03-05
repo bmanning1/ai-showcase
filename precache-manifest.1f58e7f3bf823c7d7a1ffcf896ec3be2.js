a02b526086e894a281982fa83f14c31b91388513
Becca Manning <rebm01@hotmail.co.uk>
2020-03-05 22:46:27 +0000

    Updates

---
+++ precache-manifest.1f58e7f3bf823c7d7a1ffcf896ec3be2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e8c9775c63750c6db7daa280fb6787",
+    "url": "/ai-showcase/index.html"
+  },
+  {
+    "revision": "008bd3d8c813803bdfb2",
+    "url": "/ai-showcase/static/css/main.20da1abc.chunk.css"
+  },
+  {
+    "revision": "d70a626eea6c50fc49c8",
+    "url": "/ai-showcase/static/js/2.9548764c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ai-showcase/static/js/2.9548764c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "008bd3d8c813803bdfb2",
+    "url": "/ai-showcase/static/js/main.e972c7bd.chunk.js"
+  },
+  {
+    "revision": "288846e8bf7af0645c4f",
+    "url": "/ai-showcase/static/js/runtime-main.fe5ab7dd.js"
+  },
+  {
+    "revision": "979f4d70fe792f240029fe9dd9d81b86",
+    "url": "/ai-showcase/static/media/DM_TitlePage_2.979f4d70.jpg"
+  }
+]);
